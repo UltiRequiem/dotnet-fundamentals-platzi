1b81dcc8945fe77bf402e79cb4fdc466e79b3429
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-07-26 10:37:14 -0500

    :see_no_evil: Update .gitignore

--- Program.cs
+++ Program.cs
@@ -1,12 +1,19 @@
 ﻿using Humanizer;
 
 Console.WriteLine("Your name:");
+
 var name = Console.ReadLine() ?? String.Empty;
 
 Console.WriteLine("Your job:");
+
 var job = Console.ReadLine() ?? String.Empty;
 
 Console.WriteLine("Your age:");
-var age = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException() );
 
-Console.WriteLine($"My name is {name}, I'm {age.ToWords()} years old and I work as a {job}.");
+var age = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+var message = $"My name is {name}, I'm {age.ToWords()} years old and I work as a {job}.";
+
+Console.WriteLine(message)
+
+Console.WriteLine("Cheers.")
