806e81eefeb43dd547d771d25f194e758386f3ef
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-07-26 10:25:27 -0500

    :tada: Begin

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Humanizer;
+
+Console.WriteLine("Your name:");
+var name = Console.ReadLine() ?? String.Empty;
+
+Console.WriteLine("Your job:");
+var job = Console.ReadLine() ?? String.Empty;
+
+Console.WriteLine("Your age:");
+var age = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException() );
+
+Console.WriteLine($"My name is {name}, I'm {age.ToWords()} years old and I work as a {job}.");
